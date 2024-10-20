ce7691a49f55a629e780e10b2f15520b9d857969
RT567 <robbie.thompsonator@gmail.com>
2024-10-21 10:58:19 +1100

    add CORS

--- main.py
+++ main.py
@@ -2,8 +2,22 @@ from typing import Optional, List
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "*",  # For testing purposes; replace with your frontend URL in production
+    # e.g., "https://your-username.github.io",
+]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Update with your frontend domain(s)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class HoldsRequest(BaseModel):
     holds: List[str]
