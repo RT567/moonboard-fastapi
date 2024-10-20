cd7f06a3ca9e721e76fe10fa4386b19c4917a937
RT567 <robbie.thompsonator@gmail.com>
2024-10-21 10:48:54 +1100

    create hard coded test response

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from typing import Optional
+from typing import Optional, List
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class HoldsRequest(BaseModel):
+    holds: List[str]
+
+class GradeResponse(BaseModel):
+    grade: str
+
+@app.post("/grade_climb", response_model=GradeResponse)
+async def grade_climb(Request: HoldsRequest):
+    return{"grade": "V5"}
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
